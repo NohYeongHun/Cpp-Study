fbab80873f9a6121329f4fd811fc03a27ce9aff8
ny <dkzndk1100@naver.com>
2023-03-17 22:27:57 +0900

    비트 연산과 비트 플래그

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -1,72 +1,74 @@
 ﻿#include <iostream>
+#include <bitset>
 using namespace std;
 
-// a [ 1 ]
-// a라는 이름의 바구니를 할당하고 안에 1을 넣는다.
-int a = 1;
+// 오늘의 주제 : 데이터 연산
+// 데이터를 가공하는 방법에 대해서 알아봅시다
 
-// b [ 2 ]
-// b라는 이름의 바구니를 할당하고 안에 2를 넣는다.
-int b = 1;
-
-bool isSame;
-bool isDifferent;
-bool isGreater;
-bool isSmaller;
-
-bool test;
-
-int hp = 100;
-bool isinvincible = true;
+unsigned char flag; // 부호를 없애야 >> 를 하더라도 부호비트가 딸려오지 않음.
 
 int main()
 {
 
 // visual Studio 편집기
-#pragma region 비교 연산
-	
+#pragma region 비트 연산
 	// 언제 필요한가?
-	// ex) 체력이 0이 되면 사망
-	// ex) 체력이 30% 이하이면 궁극기를 발동 ( 100 * hp / maxHp)
-	// ex) 경험치가 100 이상이면 레벨업
+	// 비트 단위의 조작이 필요할 때
+	// - 대표적으로 BitFlag
 
-	// a == b : a와 b의 값이 같은가?
-	// 같으면 1, 다르면 0
-	isSame = (a == b);
+	// - bitwise not
+	// 단일 숫자의 모든 비트를 대상으로, 0은 1, 1은 0으로 뒤바뀜
+	
 
-	// a != b : a와 b의 값이 다른가?
-	isDifferent = (a != b);
+	// & bitwise and
+	// 두 숫자의 모든 비트 쌍을 대상으로, and를 한다.
 
-	// a > b : a가 b보다 큰가?
-	// a >= b : a가 b보다 크거나 같은가?
-	// 1 아니면 0
-	isGreater = (a > b);
+	// | bitwise or
+	// 두 숫자의 모든 비트 쌍을 대상으로, or을 한다.
+	
+	// ^ bitwise xor
+	// 두 숫자의 모든 비트 쌍을 대상으로, xor을 한다.
 
-	// a < b : a가 b보다 작은가?
-	// a <= b : a가 b보다 작거나 같은가?
-	// 1 아니면 0
-	isSmaller = (a < b);
+	// << 비트 좌측 이동
+	// 비트열을 N만큼 왼쪽으로 이동
+	// 왼쪽의 넘치는 N개의 비트는 버림. 새로 생성되는 N개의 비트는 0
+	// *2를 할 때 자주 보이는 패턴
+
+	// >> 비트 우측 이동
+	// 비트열을 N만큼 오른쪽으로 이동
+	// 오른쪽의 넘치는 N개의 비트는 버림.
+	// 왼쪽 생성되는 N개의 비트는 
+	// - 부호 비트가 존재할 경우 부호 비트를 따라감 (부호있는 정수라면 이 부분을 유의)
+	// - 아니면 0
+	
+	// 실습
+	// 0b0000 [무적][변이][스턴][공중부양]
+
+	// 무적 상태로 만든다
+	flag = (1 << 3);
+	// 변이 상태를 추가한다.(무적 + 변이).
+	flag |= (1 << 2);
+
+	// 스턴 상태를 추가한다.(무적 + 변이 + 스턴).
+	// flag |= (1 << 1);
+	 
+	
+
+	// 무적인지 확인하고 싶다? (다른상태는 관심 없음)
+	// bitmask
+	bool invincible = (flag & (1 << 3)) != 0; // flag & (1 << 3) => 1000 != 0 => 0이 아니므로 true
+
+	unsigned int mask = (1 << 3) | (1 << 1);
+	// 무적이거나 스턴 상태인지 확인하고 싶다면?
+	bool stunOrInvincible = ((flag & 0b1010) != 0);
+
+	unsigned int x = (1 << 3) | (1 << 2);
+	cout << bitset<4>(x) << endl;
+	bool isBool = ((x & mask) != 0);
+	cout << isBool << endl;
+
 
 
 #pragma endregion
 
-#pragma region 논리 연산
-	// 언제 필요한가? 조건에 대한 논리적 사고가 필요할 때
-	// ex) 로그인할 때 아이디도 같고 AND 비밀번호도 같아야 한다.
-	// ex) 길드 마스터이거나 OR 운영자 계정이면 길드 해산 가능.
-
-	// ! not
-	// 0이면 1, 그 외 0
-	test = !isSame; // 사실상 isDifferent의 의미
-
-	// && AND
-	// a && b -> 둘다 1이면 1, 그 외 0
-	test = (hp <= 0 && isinvincible == false); // 죽음
-
-	// || or 
-	// a || b - > 둘중 하나라도 1이라면 1, 아니면 0
-	test = (hp > 0 || isinvincible == true); // 살았음
-
-
-#pragma endregion
 }
