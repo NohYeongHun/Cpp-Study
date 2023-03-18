1d8a3814d7cf28e53a79da642ec25ef8b4e3c6f7
ny <dkzndk1100@naver.com>
2023-03-18 14:56:52 +0900

    분기문

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -1,54 +1,37 @@
 ﻿#include <iostream>
 using namespace std;
 
-// 오늘의 주제 : 유의사항
-
-// 1) 변수의 유효범위
-
-// 전역 변수 
-int hp = 10;
-
-// 스택
-// { } 중괄호의 범위가 변수의 생존 범위
-// 같은 이름을 두번 사용할 때 문제
-
-// 2) 연산의 우선순위
-
-// 3) 타입 변환
-
-// 4) 사칙 연산 관련
+// 오늘의 주제 : 분기문
+// 데이터를 메모리에 할당하고 가공하는 방법에 대해 알아봄
+// 가공한 데이터를 이용해서 무엇인가를 하고 싶다면?
 
 
 int main()
 {
-	int hp = 20;
-	cout << hp << endl;
+	const int ROCK = 0;
+	const int PAPER = 1;
+	const int SCISSORS = 2;
 
-	// 짝수 여부
-	bool isEven = (hp % 2 == 0);
+	int input = 40;
 
-	// 짝수거나 3으로 나뉘는 값인지
-	bool isEvenOrDivBy3 = (hp % 2 == 0 || hp % 3 == 0);
+	// switch-case-break
 
-	// 바구니 교체 (형식 교체)
-	short hp2 = (short)hp; // 위쪽 비트 데이터를 짤린 상태로 저장
-	float hp3 = (float)hp; // 실수로 변환할 때 정밀도 차이가 있기 때문에 데이터 손실.
-	unsigned int hp4 = (unsigned int)hp; // 비트 단위로 보면 똑같은데, 분석하는 방법이 달라짐.
+	// 정수 계열만 넣을 수 있다.
+	// float, double등 실수 타입은 넣을 수 없다.
+	switch (input)
+	{
+		case ROCK:
+			cout << "바위를 냈습니다." << endl;
+			break;
+		case PAPER:
+			cout << "보를 냈습니다." << endl;
+			break;
+		case SCISSORS:
+			cout << "가위를 냈습니다." << endl;
+			break;
+		default:
+			cout << "뭘 낸 겁니까?" << endl;
+			break;
+	}
 
-	// 곱셈
-	// - 오버 플로우
-	// 나눗셈
-	// - 0 나누기 조심
-	// - 실수 관련
-	// - 실수관련
-
-	int maxHp = 1000;
-	float ratio = hp / (float)maxHp; // 0.1999996
-	cout << ratio << endl;
-
-}
-
-void Test()
-{
-	
 }
