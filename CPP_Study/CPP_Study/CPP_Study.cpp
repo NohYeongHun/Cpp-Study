6afc3a165d1e3f93d8b7b03169d4da78d79677c4
ny <dkzndk1100@naver.com>
2023-03-18 15:29:41 +0900

    반복문

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -1,37 +1,51 @@
 ﻿#include <iostream>
 using namespace std;
 
-// 오늘의 주제 : 분기문
+// 오늘의 주제 : 반복문
 // 데이터를 메모리에 할당하고 가공하는 방법에 대해 알아봄
 // 가공한 데이터를 이용해서 무엇인가를 하고 싶다면?
 
 
 int main()
 {
-	const int ROCK = 0;
-	const int PAPER = 1;
-	const int SCISSORS = 2;
+	int round = 1;
+	int hp = 100;
+	int damage = 20;
 
-	int input = 40;
-
-	// switch-case-break
-
-	// 정수 계열만 넣을 수 있다.
-	// float, double등 실수 타입은 넣을 수 없다.
-	switch (input)
+	// 무한 루프 : 전투 시작
+	while (true)
 	{
-		case ROCK:
-			cout << "바위를 냈습니다." << endl;
+		hp -= damage;
+		if (hp < 0) 
+		{
+			hp = 0; // 음수 체력을 0으로 보정
+		}
+
+		// 시스템 메시지
+		cout << "Round " << round << " 몬스터 체력 " << hp << endl;
+
+		if (hp == 0)
+		{
+			cout << "몬스터 처치" << endl;
 			break;
-		case PAPER:
-			cout << "보를 냈습니다." << endl;
+		}
+
+		if (round == 5)
+		{
+			cout << "제한 라운드 종료" << endl;
 			break;
-		case SCISSORS:
-			cout << "가위를 냈습니다." << endl;
-			break;
-		default:
-			cout << "뭘 낸 겁니까?" << endl;
-			break;
+		}
+
+		round++;
 	}
 
+	// 1 ~ 10 사이의 홀수만 출력하기
+	for (int count = 1; count < 10; count++) {
+		bool isEven = ((count % 2) == 0);
+		if (isEven)
+		{
+			continue;
+		}
+		cout << count << endl;
+	}
 }
