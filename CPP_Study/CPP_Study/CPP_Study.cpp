065af53de0ca651e2efe99a10c08dca6616ff563
ny <dkzndk1100@naver.com>
2023-03-18 13:54:40 +0900

    const와 메모리 구조

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -1,5 +1,4 @@
 ﻿#include <iostream>
-#include <bitset>
 using namespace std;
 
 // 오늘의 주제 : 데이터 연산
@@ -7,68 +6,55 @@ using namespace std;
 
 unsigned char flag; // 부호를 없애야 >> 를 하더라도 부호비트가 딸려오지 않음.
 
+
+// 전역변수
+
+// [데이터 영역]
+// .data (초기값 있는 경우)
+int a = 2;
+
+// .bss (초기값 없는 경우)
+int b;
+
+// .rodata (읽기 전용 데이터)
+const char* msg = "Hello World";
+
+// 한번 정해지면 절대 바뀌지 않을 값들
+// constant의 약자인 const를 붙임 (변수를 상수화 한다고 함)
+// const를 붙였으면 초기값을 반드시 지정해야 함.
+// 상수는 대부분 대문자로 작성하는 코딩 컨벤션이 있음.
+// const도 바뀌지 않는 읽기전용
+// .rodata?
+// C++ 표준에서 꼭 그렇게 하라는 말이 없음
+// 그냥 컴파일러 (VS) 마음
+const int AIR = 0;
+const int STUN = 1;
+const int POLYMORPH = 2;
+const int INVINCIBLE = 3;
+
+
 int main()
 {
+	// [스택 영역]
+	int c = 3;
+	int v = 4;
 
-// visual Studio 편집기
-#pragma region 비트 연산
-	// 언제 필요한가?
-	// 비트 단위의 조작이 필요할 때
-	// - 대표적으로 BitFlag
 
-	// - bitwise not
-	// 단일 숫자의 모든 비트를 대상으로, 0은 1, 1은 0으로 뒤바뀜
-	
-
-	// & bitwise and
-	// 두 숫자의 모든 비트 쌍을 대상으로, and를 한다.
-
-	// | bitwise or
-	// 두 숫자의 모든 비트 쌍을 대상으로, or을 한다.
-	
-	// ^ bitwise xor
-	// 두 숫자의 모든 비트 쌍을 대상으로, xor을 한다.
-
-	// << 비트 좌측 이동
-	// 비트열을 N만큼 왼쪽으로 이동
-	// 왼쪽의 넘치는 N개의 비트는 버림. 새로 생성되는 N개의 비트는 0
-	// *2를 할 때 자주 보이는 패턴
-
-	// >> 비트 우측 이동
-	// 비트열을 N만큼 오른쪽으로 이동
-	// 오른쪽의 넘치는 N개의 비트는 버림.
-	// 왼쪽 생성되는 N개의 비트는 
-	// - 부호 비트가 존재할 경우 부호 비트를 따라감 (부호있는 정수라면 이 부분을 유의)
-	// - 아니면 0
-	
+#pragma region 비트연산
 	// 실습
 	// 0b0000 [무적][변이][스턴][공중부양]
 
-	// 무적 상태로 만든다
-	flag = (1 << 3);
-	// 변이 상태를 추가한다.(무적 + 변이).
-	flag |= (1 << 2);
+	// 무적 상태로 만든다.
+	flag = (1 << INVINCIBLE);
 
-	// 스턴 상태를 추가한다.(무적 + 변이 + 스턴).
-	// flag |= (1 << 1);
-	 
-	
+	// 변이 상태를 추가한다 (무적 + 변이)
+	flag |= (1 << POLYMORPH);
 
-	// 무적인지 확인하고 싶다? (다른상태는 관심 없음)
-	// bitmask
-	bool invincible = (flag & (1 << 3)) != 0; // flag & (1 << 3) => 1000 != 0 => 0이 아니므로 true
+	// 무적인지 확인하고 싶다? (다른 상태는 관심 없음)
+	bool invincible = ((flag & (1 << INVINCIBLE)) != 0);
 
-	unsigned int mask = (1 << 3) | (1 << 1);
-	// 무적이거나 스턴 상태인지 확인하고 싶다면?
+	// 무적이거나 스턴상태인지 확인하고 싶다면?
 	bool stunOrInvincible = ((flag & 0b1010) != 0);
 
-	unsigned int x = (1 << 3) | (1 << 2);
-	cout << bitset<4>(x) << endl;
-	bool isBool = ((x & mask) != 0);
-	cout << isBool << endl;
-
-
-
 #pragma endregion
-
 }
