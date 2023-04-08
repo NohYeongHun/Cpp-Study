388916560fe22e4e5a920f682ed986fe0540bb3b
ny <dkzndk1100@naver.com>
2023-04-08 16:35:18 +0900

    multiset 설명

--- CPP_Study/CPP_Study/multiset.cpp
+++ CPP_Study/CPP_Study/multiset.cpp
@@ -1,8 +1,46 @@
 #include <iostream>
 using namespace std;
-
+#include <set>
 
 int main()
 {
+
+	multiset<int> ms;
+	// 넣기
+	ms.insert(100);
+	ms.insert(100);
+	ms.insert(100);
+	ms.insert(200);
+	ms.insert(200);
+
+	// 찾기
+	multiset<int>::iterator findIt = ms.find(100);
+
+	pair<multiset<int>::iterator, multiset<int>::iterator> itPair;
+	itPair = ms.equal_range(100);
+
+	for (multiset<int>::iterator it = itPair.first; it != itPair.second; ++it)
+	{
+		cout << *it << endl;
+	}
+
+	ms.equal_range(100);
+	multiset<int>::iterator itBegin = ms.lower_bound(100);
+	multiset<int>::iterator itEnd = ms.upper_bound(100);
+	for (multiset<int>::iterator it = itBegin; it != itEnd; ++it)
+	{
+		cout << *it << endl;
+	}
+
+	// 삭제
+	ms.erase(100);
+
+	// 데이터 순회
+	for (multiset<int>::iterator it = ms.begin(); it != ms.end(); ++it)
+	{
+		cout << *it << endl;
+	}
+
+
 
 }
